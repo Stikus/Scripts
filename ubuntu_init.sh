8fdad58f4ab2ebf1e4f4b4394a71ffc05564e811
Grammatikati Konstantin <grammatikati@cspmz.ru>
2021-09-10 11:52:21 +0300

    fix

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -77,7 +77,7 @@ apt-get --yes --no-install-recommends install \
     ca-certificates \
     gpg-agent \
     && curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg \
-    && add-apt-repository "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+    && echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null \
     && apt-get update \
     && apt-get --yes --no-install-recommends install docker-ce
 
